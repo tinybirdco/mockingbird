536fa67f31ac83586245079525e5076b4ac53258
Dmytro Radchuk <radchukd@protonmail.com>
2023-03-29 15:13:46 +0200

    Update colors

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         'tb-primary': '#1FCC83',
         'tb-body': '#f9f9f9',
+        'tb-body2': '#f7f7f7',
         'tb-text': '#25283D',
         'tb-text1': '#636679',
         'tb-text2': '#A5A7B4',

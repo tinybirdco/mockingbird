720dbe7804966cf93d369d1b47d3a035b5bd858b
Pablo Abella <pablo.abella@tinybird.co>
2024-09-20 12:07:13 +0200

    Recover host to rewrite option

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,12 +5,12 @@ const nextConfig = {
     return [
       {
         source: '/docs',
-        // has: [{ type: 'host', value: 'mockingbird.tinybird.co' }],
+        has: [{ type: 'host', value: 'mockingbird.tinybird.co' }],
         destination: 'https://mockingbird-docs.tinybird.co',
       },
       {
         source: '/docs/:path*',
-        // has: [{ type: 'host', value: 'mockingbird.tinybird.co' }],
+        has: [{ type: 'host', value: 'mockingbird.tinybird.co' }],
         destination: 'https://mockingbird-docs.tinybird.co/:path*',
       },
     ]

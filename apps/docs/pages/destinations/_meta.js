45c0b124cdddecaa563476f4ecc786d0467b9ed7
al <me@alasdairb.com>
2024-12-15 21:46:36 +0000

    move docs to readme

--- apps/docs/pages/destinations/_meta.js
+++
@@ -1,9 +0,0 @@
-export default {
-  tinybird: "Tinybird",
-  ably: "Ably",
-  aws_sns: "AWS SNS",
-  aws_kinesis: "AWS Kinesis",
-  confluent_cloud_kafka: "Confluent Cloud",
-  google_spanner: "Google Spanner",
-  rabbitmq: "RabbitMQ",
-};
